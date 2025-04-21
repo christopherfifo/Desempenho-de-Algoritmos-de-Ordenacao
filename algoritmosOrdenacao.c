8a5c6ca74497a3ac6ce374bfd59fc5ccad281278
Diego Vitali <drochavitali@gmail.com>
2025-04-20 22:50:48 -0300

    Update algoritmosOrdenacao.c
    Recolocado funções de tempo

--- algoritmosOrdenacao.c
+++ algoritmosOrdenacao.c
@@ -2,6 +2,21 @@
 #include <stdlib.h>
 #include "algoritmosOrdenacao.h"
 
+int elementoDaestrutura = 0;
+
+struct timeval{
+    long int tv_sec; 
+    long int tv_usec; 
+};   
+struct Algoritimo{
+    char nome[40];
+    int tamanho;
+    float tempoTotal;
+    float melhorCaso;
+    float piorCaso;
+};
+
+struct Algoritimo Algoritimos[10]; 
 
 //////// COUNTING SORT
 void countingSort(int inputArray[], int numElementos) {
@@ -381,5 +396,42 @@ void selectionSort(int arr[], int n) {
             arr[minIndex] = temp;
         }
     }
+}
+
+
+void salvaResultados(char nome[], int tamanho, double tempoTotal, double melhorCaso, double piorCaso){
+    if (elementoDaestrutura >= 10) {
+        printf("Limite de algoritmos atingido.\n");
+        return -1;
+    }
+
+    strcpy(Algoritimos[elementoDaestrutura].nome, nome); 
+    Algoritimos[elementoDaestrutura].tamanho = tamanho;
+    Algoritimos[elementoDaestrutura].tempoTotal = tempoTotal;
+    Algoritimos[elementoDaestrutura].melhorCaso = melhorCaso;
+    Algoritimos[elementoDaestrutura].piorCaso = piorCaso;
+
+    elementoDaestrutura++;
+};
+
+void exibeTodosOsAlgoritmos(){
+    printf("Algoritmos Salvos:\n");
+    for (int i = 0; i < 10; i++){
+        printf("---------------ALGORITIMOS--------------------\n");
+        exibirAlgoritmo(Algoritimos[i].nome, Algoritimos[i].tamanho, Algoritimos[i].tempoTotal, Algoritimos[i].melhorCaso, Algoritimos[i].piorCaso);
+    }
+};
+
+double mediaTempo(int *v, int tamanho){
+
+    double totalTempo = 0.0;
+
+    for(int i = 0; i < tamanho; i++){
+       totalTempo += v[i];
+    }
+    
+    totalTempo = totalTempo / tamanho;
+
+    return totalTempo;
 }
 
